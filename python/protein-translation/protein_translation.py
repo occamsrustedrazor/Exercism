7c6ec9e3d20ecab43ad5e4f81ccf57fc940b8f32
wing117 <axboyd72@gmail.com>
2020-09-09 10:54:02 -0400

    protein_translate updated

--- python/protein-translation/protein_translation.py
+++ python/protein-translation/protein_translation.py
@@ -1,2 +1,25 @@
+protien_dict ={
+    "Methionine":"AUG",
+    "Phenylalanine":["UUU","UUC"],
+    "Leucine":["UUA","UUG"],
+    "Serine":["UCU","UCC","UCA","UCG"],
+    "Tyrosine":["UAU","UAC"],
+    "Cysteine":["UGU","UGC"],
+    "Tryptophan":"UGG",
+    "STOP":["UAA","UAG","UGA"]
+}
+
 def proteins(strand):
-    pass
+    protien_list = []
+    if len(strand) % 3 != 0:
+        return "Incomplete RNA Strand Input (not divisible by 3)."
+    else:
+        codon_list = [strand[i:i+3] for i in range(0,len(strand), 3)]
+
+    for item in codon_list:
+        if item in protien_dict["STOP"]:
+            return protien_list
+        for protien_name,codon in protien_dict.items():
+            if item in codon:
+                protien_list.append(protien_name)
+    return protien_list
