58a8cb4759385ff869cae9efd3fe174d22255bd9
wing117 <axboyd72@gmail.com>
2020-09-11 10:14:09 -0400

    simplified if statements

--- python/matrix/matrix.py
+++ python/matrix/matrix.py
@@ -1,21 +1,12 @@
 class Matrix:
     def __init__(self, matrix_string):
-        split_matrix_string = (matrix_string.split('\n'))
-        
-        the_matrix = []
-        self.the_matrix = the_matrix
+        self.matrix = matrix_string.split('\n')
 
-        for i in range(0,len(split_matrix_string)):
-            the_matrix.append(split_matrix_string[i].split())
-            for j in range(0,len(the_matrix[i])):
-                the_matrix[i][j] = int(the_matrix[i][j])
+        for i in range(0,len(self.matrix)):
+            self.matrix[i] = self.matrix[i].split()
 
     def row(self, index):
-        row = self.the_matrix[index-1]
-        return row
+        return [int(row) for row in self.matrix[index-1]]
 
     def column(self, index):
-        columns = []
-        for col in self.the_matrix:
-            columns.append(col[index-1])
-        return columns
+        return [int(col[index-1]) for col in self.matrix]
